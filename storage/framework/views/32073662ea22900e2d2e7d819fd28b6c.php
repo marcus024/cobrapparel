3b46993d7bd71f03fb692a486fbd6a071aaa63fe
Mark Antony Calipayan <markantony.calipayan@ssu.edu.ph>
2025-03-26 01:38:47 +0800

    added favicon

--- storage/framework/views/32073662ea22900e2d2e7d819fd28b6c.php
+++ storage/framework/views/32073662ea22900e2d2e7d819fd28b6c.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Checkout - Cobrapparel</title>
+    <link rel="icon" href="cobra.png" type="image/x-icon">
     <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
